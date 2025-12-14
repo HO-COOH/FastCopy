91c863a2d491d0092bf12778ebf9b6f781e0cc4f
Peter <szpeter@Yeah.net>
2025-12-14 21:34:01 +0800

    Feat: Add robocopy check

--- FastCopy/WelcomeWindow.xaml.cpp
+++ FastCopy/WelcomeWindow.xaml.cpp
@@ -1,4 +1,4 @@
-#include "pch.h"
+﻿#include "pch.h"
 #include "WelcomeWindow.xaml.h"
 #if __has_include("WelcomeWindow.g.cpp")
 #include "WelcomeWindow.g.cpp"
@@ -7,6 +7,8 @@
 #include "Global.h"
 #include "WindowHelper.h"
 #include <PackageConfig.h>
+#include <wil/win32_helpers.h>
+#include <filesystem>
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -20,6 +22,7 @@ namespace winrt::FastCopy::implementation
 			.Width = PackageConfig::GetDefaultLanguage() == L"zh-cn" ? 500 : 600,
 			.Height = 280
 		});
+		checkRobocopy();
 	}
 
 	void WelcomeWindow::Button_Click(winrt::Windows::Foundation::IInspectable const&, winrt::Microsoft::UI::Xaml::RoutedEventArgs const&)
@@ -35,5 +38,34 @@ namespace winrt::FastCopy::implementation
 		winrt::Microsoft::UI::Xaml::RoutedEventArgs const&)
 	{
 		system("taskkill /F /IM explorer.exe & start explorer");
+	}
+	winrt::FastCopy::RobocopyCheckResult WelcomeWindow::CheckResult()
+	{
+		return m_checkResult;
+	}
+
+	void WelcomeWindow::checkResult(winrt::FastCopy::RobocopyCheckResult value)
+	{
+		m_checkResult = value;
+		raisePropertyChange(L"CheckResult");
+	}
+
+	void WelcomeWindow::checkRobocopy()
+	{
+		//check if robocopy.exe exist in %systemroot%\System32
+		if (!std::filesystem::exists(wil::ExpandEnvironmentStringsW(LR"(%systemroot%\System32\robocopy.exe)").get()))
+		{
+			checkResult(winrt::FastCopy::RobocopyCheckResult::NotFound);
+			return;
+		}
+
+		//check if english resource exist in %systemroot%\System32\en-US\robocopy.exe.mui
+		if (!std::filesystem::exists(wil::ExpandEnvironmentStringsW(LR"(%systemroot%\System32\en-US\robocopy.exe.mui)").get()))
+		{
+			checkResult(winrt::FastCopy::RobocopyCheckResult::NoEnglishResource);
+			return;
+		}
+
+		checkResult(winrt::FastCopy::RobocopyCheckResult::Normal);
 	}
 }
--- FastCopy/WelcomeWindow.xaml.h
+++ FastCopy/WelcomeWindow.xaml.h
@@ -1,14 +1,21 @@
-#pragma once
+﻿#pragma once
 
 #include "WelcomeWindow.g.h"
+#include <include/PropertyChangeHelper.hpp>
 
 namespace winrt::FastCopy::implementation
 {
-    struct WelcomeWindow : WelcomeWindowT<WelcomeWindow>
+    struct WelcomeWindow : WelcomeWindowT<WelcomeWindow>, MvvmHelper::PropertyChangeHelper<WelcomeWindow>
     {
         WelcomeWindow();
         void Button_Click(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::RoutedEventArgs const& e);
         void KillExplorerButton_Click(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::RoutedEventArgs const& e);
+
+        winrt::FastCopy::RobocopyCheckResult CheckResult();
+    private:
+        winrt::FastCopy::RobocopyCheckResult m_checkResult = winrt::FastCopy::RobocopyCheckResult::Checking;
+        void checkResult(winrt::FastCopy::RobocopyCheckResult value);
+        void checkRobocopy();
     };
 }
 
