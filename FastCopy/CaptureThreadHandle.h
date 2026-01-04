0d45368fbb2773a1ab314d45a5529da979c7b97e
HO-COOH <szpeter@yeah.net>
2026-01-04 22:37:14 +0800

    Chore: Use manifest mode for vcpkg. Compatibility with boost 1.87+. Closes #40

--- FastCopy/CaptureThreadHandle.h
+++ FastCopy/CaptureThreadHandle.h
@@ -1,5 +1,9 @@
 ﻿#pragma once
+#if __has_include("boost/process/v1/extend.hpp")
+#include <boost/process/v1/extend.hpp>
+#else
 #include <boost/process/extend.hpp>
+#endif
 #include <wil/resource.h>
 #include <Windows.h>
 
@@ -7,7 +11,12 @@
 * @brief This class is needed for capturing the `PROCESS_INFORMATION`, because `boost::process` (v1 to be exact)
 		 does not have a getter for it
 */
-struct CaptureThreadHandle : boost::process::extend::handler
+struct CaptureThreadHandle : 
+#if __has_include("boost/process/v1/extend.hpp")
+	boost::process::v1::extend::handler
+#else
+	boost::process::extend::handler
+#endif
 {
 	wil::unique_handle& m_hThread;
 	CaptureThreadHandle(wil::unique_handle& hThread) : m_hThread(hThread) {}
--- RobocopyInjection/dllmain.cpp
+++ RobocopyInjection/dllmain.cpp
@@ -1,4 +1,4 @@
-﻿#include "pch.h"
+#include "pch.h"
 #pragma comment(lib, "Dbghelp.lib")
 
 // Configuration: The Language ID we want to force (e.g., en-US)
@@ -7,10 +7,12 @@ constexpr static auto TargetLangId = 0x0409;
 // Define the function signatures we are hooking
 using GetUserDefaultLanguageFunc = LANGID(WINAPI*)(void);
 using SetThreadUILanguageFunc = LANGID(WINAPI*)(LANGID);
+using GetThreadUILanguageFunc = LANGID(WINAPI*)(void);
 
 // Pointer to store the original address (optional for IAT, but good practice)
 GetUserDefaultLanguageFunc pOriginalGetUserDefaultUILanguage{};
 SetThreadUILanguageFunc pOriginalSetThreadUILanguage{};
+GetThreadUILanguageFunc pOriginalGetThreadUILanguage{};
 
 // ---------------------------------------------------------
 // 1. The Fake Function
@@ -21,6 +23,11 @@ LANGID WINAPI Fake_GetUserDefaultUILanguage(void)
 }
 
 LANGID WINAPI Fake_SetThreadUILanguage(LANGID LangId) 
+{
+    return TargetLangId;
+}
+
+LANGID WINAPI Fake_GetThreadUILanguage(void) 
 {
     return TargetLangId;
 }
@@ -43,6 +50,7 @@ void InstallIATHook(const char* dllName, const char* funcName, void* newFuncAddr
     while (pImportDesc->Name) {
         const char* moduleName = (char*)((BYTE*)hModule + pImportDesc->Name);
 
+        // Check if the module name matches (case insensitive)
         if (_stricmp(moduleName, dllName) == 0) {
             // Found the DLL. Now loop through its functions.
 
@@ -51,7 +59,12 @@ void InstallIATHook(const char* dllName, const char* funcName, void* newFuncAddr
             // FirstThunk points to the function ADDRESSES (The IAT)
             PIMAGE_THUNK_DATA pThunkAddr = (PIMAGE_THUNK_DATA)((BYTE*)hModule + pImportDesc->FirstThunk);
 
-            while (pThunkName->u1.Function) {
+            // Some compilers might not produce OriginalFirstThunk. If so, use FirstThunk (which points to names before binding)
+            if (!pThunkName) {
+                pThunkName = pThunkAddr;
+            }
+
+            while (pThunkName && pThunkName->u1.Function) {
                 // Check if imported by Name (not by Ordinal)
                 if (!(pThunkName->u1.Ordinal & IMAGE_ORDINAL_FLAG)) {
                     PIMAGE_IMPORT_BY_NAME pImportName = (PIMAGE_IMPORT_BY_NAME)((BYTE*)hModule + pThunkName->u1.AddressOfData);
@@ -75,7 +88,7 @@ void InstallIATHook(const char* dllName, const char* funcName, void* newFuncAddr
 
                         // 4. Restore memory protections
                         VirtualProtect(&pThunkAddr->u1.Function, sizeof(uintptr_t), oldProtect, &oldProtect);
-
+                        
                         return; // Hook installed, we are done.
                     }
                 }
@@ -94,16 +107,31 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReser
     if (ul_reason_for_call == DLL_PROCESS_ATTACH) {
         DisableThreadLibraryCalls(hModule);
 
+        // Force thread language immediately using the actual API (if available)
+        HMODULE hKernel = GetModuleHandleA("kernel32.dll");
+        if (hKernel) {
+             SetThreadUILanguageFunc pSet = (SetThreadUILanguageFunc)GetProcAddress(hKernel, "SetThreadUILanguage");
+             if (pSet) {
+                 pSet(TargetLangId);
+             }
+        }
+
         // Hook GetUserDefaultUILanguage inside kernel32.dll
         InstallIATHook("kernel32.dll", "GetUserDefaultUILanguage", Fake_GetUserDefaultUILanguage, (void**)&pOriginalGetUserDefaultUILanguage);
+        InstallIATHook("kernelbase.dll", "GetUserDefaultUILanguage", Fake_GetUserDefaultUILanguage, NULL);
 
         // Often apps call GetSystemDefaultUILanguage as fallback, hook that too
         InstallIATHook("kernel32.dll", "GetSystemDefaultUILanguage", Fake_GetUserDefaultUILanguage, NULL);
+        InstallIATHook("kernelbase.dll", "GetSystemDefaultUILanguage", Fake_GetUserDefaultUILanguage, NULL);
 
         // Hook SetThreadUILanguage to force en-US
         InstallIATHook("kernel32.dll", "SetThreadUILanguage", Fake_SetThreadUILanguage, (void**)&pOriginalSetThreadUILanguage);
+        InstallIATHook("kernelbase.dll", "SetThreadUILanguage", Fake_SetThreadUILanguage, NULL);
 
-        //MessageBox(NULL, L"Injected", L"", 0);
+        // Hook GetThreadUILanguage
+        InstallIATHook("kernel32.dll", "GetThreadUILanguage", Fake_GetThreadUILanguage, (void**)&pOriginalGetThreadUILanguage);
+        InstallIATHook("kernelbase.dll", "GetThreadUILanguage", Fake_GetThreadUILanguage, NULL);
     }
     return TRUE;
 }
+
